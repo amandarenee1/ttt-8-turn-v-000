32d0f72bd216b2e54d70f39baeca98d400028629
Amanda Wingate <arw0219@gmail.com>
2018-01-21 02:50:06 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,6 +8,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+#converts user's place on board to index integer
 def input_to_index(user_input)
   user_input.to_i-1
 end
