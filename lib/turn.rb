5e50bde071f4da63d50dff29e5d2db82021926e1
Amanda Wingate <arw0219@gmail.com>
2018-01-21 02:59:04 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -42,7 +42,6 @@ end
 
 #asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
 def turn(board)
-
   puts "Please enter 1-9:"
-  input_to_index = gets.strip
+  num = gets.chomp
 end
