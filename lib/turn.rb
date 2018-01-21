7b93812486f904c5246f2659cae29e0ba6ac1f5a
Amanda Wingate <arw0219@gmail.com>
2018-01-21 01:34:50 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,6 +40,7 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
      move(board, position, char="X")
+     position_taken?(board, position)
      turn(board)
    end
   display_board(board)
